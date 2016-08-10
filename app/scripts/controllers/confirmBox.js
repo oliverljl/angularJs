448cf85e8079e74db1570c25d797888380a37c61
oliverljl <oliverljl@hotmail.com>
2016-08-10 13:36:41 -0400

    delete node_module

--- app/scripts/controllers/confirmBox.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-// CONFIRMBOX CONTROLLER
-// Description: Define the following functionalities: 
-// Delete items for "Delete" button
-// Close the dialog modal for "Cancel" button
--- app/scripts/controllers/producer.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-// PRODUCER CONTROLLER
-// Description: Define the following functionalities:
-// Creative manipulations for whatever stuff this section wants to do
